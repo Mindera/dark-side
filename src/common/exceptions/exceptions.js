71e677e313652444386f93d48c978399ef32c688
Bruno Palos <bruno.palos@mindera.com>
2015-06-29 16:50:15 +0100

    Adding type field to exceptions objects.

--- src/common/exceptions/exceptions.js
+++ src/common/exceptions/exceptions.js
@@ -2,14 +2,16 @@ module.exports = {
     InvalidArgumentsException: function (message) {
         return {
             name: 'Invalid arguments exception',
-            message: message
+            message: message,
+            type: 'INVALID_ARGUMENTS'
         };
     },
 
     NotFoundException: function (message) {
         return {
             name: 'Not found exception',
-            message: message
+            message: message,
+            type: 'NOT_FOUND'
         };
     }
 };
